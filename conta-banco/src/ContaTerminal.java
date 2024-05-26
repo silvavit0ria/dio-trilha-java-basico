075de34c321cdf23eecbb8b5d4c31056f750994b
vitoria silva <vitoriafontessilva32@gmail.com>
2024-05-26 16:20:58 -0300

    simulando uma conta de banco com interação  usuario com java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+          
+        //criando uma conta bancaria basica com java.//
+        Scanner scanner = new Scanner(System.in); // variavel que solicita o usuario a digitar 
+
+        System.out.println("Digite o número da Agência:"); //solicite o user a colocar suas informações
+        String agencia = scanner.nextLine(); //variavel scanner + nextLine para a proxima solicitação 
+
+        System.out.println("Digite o número da Conta:");
+        int numero = scanner.nextInt(); //atenção no tipo da variavel!!
+        scanner.nextLine(); 
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        scanner.close(); //fechando o campo de solicitação 
+        
+        System.out.println("Olá " + nome + ", obrigado por criar sua conta conosco ");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
     }
 }
+
+
+
+    
+
